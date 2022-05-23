83b0735ee4eb5ab7f946dc7cf431837f5c30deac
YunjuChoi <yunju00791@kookmin.ac.kr>
2022-05-23 12:03:38 +0900

    modified image path

--- template.py
+++ template.py
@@ -2,16 +2,13 @@ html = b"""
 <html>
     <body>
         <form action="">
-            x = <input type="number" name="a"><br><br>
-            y = <input type="number" name="b"><br><br>
-            
+            y = <input type="number" name="a"> * x^2
+            + <input type="number" name="b"> * x
+            + <input type="number" name="c"><br><br>
             <input type="submit">
 
         </form>
-        <p>
-            x + y: %(x + y)s<br>
-            x * y: %(x * y)s<br>
-        </p>
+        <img src="/img/graph.png" alt="Result image here">
         
     </body>
 </html>
